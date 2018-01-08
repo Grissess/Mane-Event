61438ec4fffac480cedb3e63b039ef6433dcd3b3
Graham Northup <grissess@nexusg.org>
2018-01-08 14:51:49 -0500

    Fixed wall calendar timers

--- bot.py
+++ bot.py
@@ -1,14 +1,4 @@
-import discord
-import asyncio
-import re
-import os
-import datetime
-import traceback
-import random
-import threading
-import asyncio
-from datetime import datetime
-from threading import Timer
+import discord, asyncio, re, os, datetime, traceback, random, threading, asyncio
 
 client = discord.Client()
 
@@ -25,24 +15,30 @@ async def on_ready():
 	for server in client.servers:
 		print(server.name)
 
-x=datetime.today()
-y=x.replace(day=x.day+1, hour=13, minute=45, second=20, microsecond=0)
-delta_t=y-x
+goal_time = datetime.time(14, 49, 30)
+now = datetime.datetime.now()
+goal = now.replace(day=now.day, hour=goal_time.hour, minute=goal_time.minute, second=goal_time.second, microsecond=0)
 
-#secs=delta_t.seconds+1
-secs=60
+loop = asyncio.get_event_loop()
 
 async def good_morning():
-	msg = random.choice(['Good Morning Baltimare!', 'It\'s a lovely day in Baltimare!'])
-	await client.send_message(discord.Object(id='370664588167086090'), msg)
-	print('Good Morning!')
+	global goal
 
-def do_good_morning():
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(good_morning())
+	now = datetime.datetime.now()
+	print('check', (now.year, now.month, now.day, now.hour, now.minute, now.second), (goal.year, goal.month, goal.day, goal.hour, goal.minute, goal.second))
+	# Check for inequality down to the second
+	if (now.year, now.month, now.day, now.hour, now.minute, now.second) >= (goal.year, goal.month, goal.day, goal.hour, goal.minute, goal.second):
+		msg = random.choice(['Good Morning Baltimare!', 'It\'s a lovely day in Baltimare!'])
+		await client.send_message(discord.Object(id='370664588167086090'), msg)
+		print('Good Morning!')
+		# Prepare the next goal time
+		goal = now.replace(day=now.day + 1, hour=goal_time.hour, minute=goal_time.minute, second=goal_time.second, microsecond=0)
 
-t = Timer(secs, do_good_morning)
-t.start()
+	# In all cases, ensure this is rescheduled (0.5 might be a little eager, but the value does have to be strictly <1 for second accuracy)
+	loop.call_later(0.5, asyncio.ensure_future, good_morning())
+
+# Schedule the first run to kick off the loop
+loop.call_soon(asyncio.ensure_future, good_morning())
 
 # string a is words to delete
 BannedWords = ['ahole', 'anus', 'ash0le', 'ash0les', 'asholes', 'ass ', ' ass', 'assmonkey', 'assface', 'assh0le', 'assh0lez', 'asshole', 'asshole', 'assholes', 'assholz', 'asswipe', 'azzhole', 'bassterds', 'bastard', 'bastards', 'bastardz', 'basterds', 'basterdz', 'biatch', 'bitch', 'bitches', 'blowjob', 'boffing', 'bullshit', 'butthole', 'buttwipe', 'c0ck', 'c0cks', 'c0k', 'carpetmuncher', 'cawk', 'cawks', 'clit', 'cnts', 'cntz', 'cock', 'cockhead', 'cockhead', 'cocks', 'cocksucker', 'cocksucker', 'cum ', ' cum', 'cunt', 'cunts', 'cuntz', 'dick', 'dild0', 'dild0s', 'dildo', 'dildos', 'dilld0', 'dilld0s', 'dominatricks', 'dominatrics', 'dominatrix', 'dyke', 'enema', 'fag', 'fag1t', 'faget', 'fagg1t', 'faggit', 'faggot', 'fagit', 'fags', 'fagz', 'faig', 'faigs', 'fart', 'flipping', 'fuck', 'fucker', 'fuckin', 'fucking', 'fucks', 'fudgepacker', 'fuk', 'fukah', 'fuken', 'fuker', 'fukin', 'fukk', 'fukkah', 'fukken', 'fukker', 'fukkin', 'g00k', 'gay', 'gaybor', 'gayboy', 'gaygirl', 'gays', 'gayz', 'goddamned', 'h00r', 'h0ar', 'h0re', 'hells', 'hoar', 'hoor', 'hoore', 'jackoff', 'jap', 'japs', 'jerkoff', 'jisim', 'jiss', 'jizm', 'jizz', 'knob', 'knobs', 'knobz', 'kunt', 'kunts', 'kuntz', 'lesbian', 'lezzian', 'lipshits', 'lipshitz', 'masochist', 'masokist', 'massterbait', 'masstrbait', 'masstrbate', 'masterbaiter', 'masterbate', 'masterbates', 'mutha', 'fuker', 'motha', 'fucker', 'fuker', 'fukka', 'fukkah', 'fucka', 'fuchah', 'fukker', 'fukah', 'mothafucker', 'mothafuker', 'mothafukkah', 'mothafukker', 'motherfucker', 'motherfukah', 'motherfuker', 'motherfukkah', 'motherfukker', 'motherfucker', 'muthafucker', 'muthafukah', 'muthafuker', 'muthafukkah', 'muthafukker', 'mutha', 'n1gr', 'nastt', 'nasty', 'nigger', 'nigur', 'niiger', 'niigr', 'orafis', 'orgasim', 'orgasm', 'orgasum', 'oriface', 'orifice', 'orifiss', 'packi', 'packie', 'packy', 'paki', 'pakie', 'paky', 'pecker', 'peeenus', 'peeenusss', 'peenus', 'peinus', 'pen1s', 'penas', 'penis', 'penisbreath', 'penus', 'penuus', 'phuc', 'phuck', 'phuk', 'phuker', 'phukker', 'polac', 'polack', 'polak', 'poonani', 'pr1c', 'pr1ck', 'pr1k', 'pusse', 'pussee', 'pussy', 'puuke', 'puuker', 'queer', 'queers', 'queerz', 'qweers', 'qweerz', 'qweir', 'recktum', 'rectum', 'retard', 'sadist', 'scank', 'schlong', 'screwing', 'semen', 'sex', 'sexx', 'sexxx', 'sx', 'sexy', 'sht', 'sh1t', 'sh1ter', 'sh1ts', 'sh1tter', 'sh1tz', 'shit', 'shits', 'shitter', 'shitty', 'shity', 'shitz', 'shyt', 'shyte', 'shytty', 'shyt', 'skanck', 'skank', 'skankee', 'skankey', 'skanks', 'skanky', 'slut', 'sluts', 'slutty', 'slutz', 'sonofabitch', 'tit', 'turd', 'va1jina', 'vag1na', 'vagiina', 'vagina', 'vaj1na', 'vajina', 'vullva', 'vulva', 'w0p', 'wh00r', 'wh0re', 'whore', 'xrated', 'xxx', 'bch', 'bitch', 'blowjob', 'clit', 'arschloch', 'fuck', 'shit', 'ass', 'asshole', 'btch', 'b17ch', 'b1tch', 'bastard', 'bich', 'boiolas', 'buceta', 'c0ck', 'cawk', 'chink', 'cipa', 'clits', 'cock', 'cum', 'cunt', 'dildo', 'dirsa', 'ejakulate', 'fatass', 'fcuk', 'fuk', 'fux0r', 'hoer', 'hore', 'jism', 'kawk', 'l3itch', 'l3i+ch', 'lesbian', 'masturbate', 'masterbat', 'masterbat3', 'motherfucker', 's.o.b.', 'mofo', 'nazi', 'nigga', 'niggas', 'nigger', 'nutsack', 'phuck', 'pimpis', 'pusse', 'pussy', 'scrotum', 'shemale', 'shi+', 'shitt', 'slut', 'smut', 'teets', 'tits', 'boobs', 'b00bs', 'teez', 'testical', 'testicle', 'titt', 'w00se', 'jackoff', 'wank', 'whoar', 'whore', 'damn', 'dyke', 'fuck', 'shit', '@$$', 'amcik', 'andskota', 'arse', 'assrammer', 'ayir', 'bi7ch', 'bitch', 'bollock', 'breasts', 'buttpirate', 'cabron', 'cazzo', 'chraa', 'chuj', 'cock', 'cunt', 'd4mn', 'daygo', 'dego', 'dick', 'dike', 'dupa', 'dziwka', 'ejackulate', 'ekrem', 'ekto', 'enculer', 'faen', 'fag', 'fanculo', 'fanny', 'feces', 'feg', 'felcher', 'ficken', 'fitt', 'flikker', 'foreskin', 'fotze', 'fu', 'fuk', 'futkretzn', 'gay', 'gook', 'guiena', 'h0r', 'h4x0r', 'hell', 'helvete', 'hoer', 'honkey', 'huevon', 'hui', 'injun', 'jizz', 'kanker', 'kike', 'klootzak', 'kraut', 'knulle', 'kuk', 'kuksuger', 'kurac', 'kurwa', 'kusi', 'kyrpa', 'lesbo', 'mamhoon', 'masturbat', 'merd', 'mibun', 'monkleigh', 'mouliewop', 'muie', 'mulkku', 'muschi', 'nazis', 'nepesaurio', 'nigger', 'orospu', 'paska', 'perse', 'picka', 'pierdol', 'pillu', 'pimmel', 'piss', 'pizda', 'poontsee', 'poop', 'porn', 'p0rn', 'pr0n', 'preteen', 'pula', 'pule', 'puta', 'puto', 'qahbeh', 'queef', 'rautenberg', 'schaffer', 'scheiss', 'schlampe', 'schmuck', 'screw', 'sharmuta', 'sharmute', 'shipal', 'shiz', 'skrib']
