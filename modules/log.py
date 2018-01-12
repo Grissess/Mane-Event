71499899f8f5b32699010b9976d5283cf881aac7
Graham Northup <grissess@nexusg.org>
2018-01-12 16:01:37 -0500

    Added file option to log module

--- modules/log.py
+++ modules/log.py
@@ -1,15 +1,25 @@
 import bot
 
 class Module(bot.Module):
+    def __init__(self, config):
+        super().__init__(config)
+
+        if 'file' in self.config:
+            self.file = open(self.config['file'], 'a')
+
+    def print(self, *args):
+        print(*args)
+        self.file.write(' '.join(args))
+
     async def on_ready(self):
-        print('log: ready')
+        self.print('log: ready')
 
     async def on_message(self, message):
-        print('log: on_message', f'#{message.channel.name} <{message.author.name}>) message.content')
+        self.print('log: on_message', f'#{message.channel.name} <{message.author.name}>) message.content')
 
     async def on_any_message(self, message):
-        print('log: on_any_message',  f'#{message.channel.name} <{message.author.name}>) message.content')
+        self.print('log: on_any_message',  f'#{message.channel.name} <{message.author.name}>) message.content')
 
     async def on_occasion(self):
         if self.config.get('occasions'):
-            print('log: on_occasion')
+            self.print('log: on_occasion')
